cf47af4cc1dbd80176c52487f1f47f07466cc995
tptam <roseisroseisrose@gmail.com>
2024-06-11 12:41:01 -0400

    Complete Optional Hands-on Lab: Create a Standalone Django ORM Project Template

---
+++ settings.py
@@ -0,0 +1,15 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'MTk5Mi1mcmllZGZy',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+INSTALLED_APPS = (
+    'standalone',
+)
+SECRET_KEY = 'SECRET KEY for this Django Project'
